d62ef8269dbd382a06be791795316c8c468f33b2
Constantine <faustlarsen@gmail.com>
2020-12-27 12:58:25 -0700

    add date and time forms

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -1,10 +1,15 @@
+using System;
+
 namespace HairSalon.Models
+
 {
     public class Client
     {
         public int ClientId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public DateTime Appointment { get; set; }
+        public int Time { get; set; }
         public int StylistId { get; set; }
         public virtual Stylist Stylist { get; set; }
     }
